5cae3e4ae6449b4734daf56a83a82fdd471fda1b
aurelabode <lelabode@hotmail.com>
2023-02-26 12:49:00 +0100

    rails new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,29 @@
 class MoviesController < ApplicationController
-  def title:string
+  def change
+    @movies = Movie.all
+    @movie = Movie.new
   end
 
-  def overview:text
+  def def new
+    @movies = Movie.new
+  end
+  def create
+    @movie = Movie.new(list_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new, status: :unprocessable_entity
+    end
+    def destroy
+      @movie = movie.find(params[:id])
+      @movie.destroy
+      redirect_to movie_path(@movie), status: :see_other
+    end
+    private
+
+  def set_movie
+    @movie = set_moveie.find(params[:movie_id])
   end
 
-  def poster_url:string
-  end
+
 end
